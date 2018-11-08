6c660bc63578b9845b51fb9f5112635763c26518
Loann_GGER <loann.goerger@glk-group.com>
2018-11-08 15:35:47 +0100

    Remplacement des multi if() par un switch

--- index.php
+++ index.php
@@ -8,27 +8,95 @@ use Controller\FrontendController;
 $frontend = new FrontendController();
 $backend = new BackendController();
 
-if (isset($_GET['action'])) {
-    if ($_GET['action'] == 'admin')
+if (isset($_GET['action'])) 
+{
+    switch ($_GET['action'])
     {
-        $backend->admin();
+        case 'admin':
+            $backend->admin();
+            break;
+
+        case 'writeNewPost':
+            $backend->addPost();
+            break;
+
+        case 'managePosts':
+            $backend->managePosts();
+            break;
+
+        case 'manageComments':
+            $backend->manageComments();
+            break;
+
+        case 'post':
+            $backend->postPost();
+            break;
+
+        case 'login':
+            $backend->login();
+            break;
+
+        case 'register':
+            $backend->register();
+            break;
+
+        case 'forgotPassword':
+            $backend->forgotPassword();
+            break;  
+
+        case 'singlepost':
+            $frontend->singlePost();
+            break;
+
+        case 'testpost':
+            $backend->show();
+            break;
+
+        case 'delete':
+            $backend->deletePost();
+            break;
+
+        case 'blog':
+            $frontend->blog();
+            break;
+
+        case'author':
+            $frontend->author();
+            break;
+
+        case 'viewPosts':
+            $backend->viewPosts();
+            break;
+
+        case 'voirlesposts':
+            $backend->voirlesposts();
+            break;
+
+        case 'updatePosts':
+            $backend->updatePosts();
+            break;
+
+        case 'writeUpdatePosts':
+            $backend->writeUpdatePosts();
+            break;
+
+        case 'delete':
+            $backend->deletePost();
+            break;
+
+        case 'viewdelete':
+            $backend->viewdelete();
+            break;
+
+        case 'lireunpost':
+            $backend->show(2);
+            break;
+
+        default:
+            $frontend->listPosts();
+            break;
     }
-    if ($_GET['action'] == 'writeNewPost')
-    {
-        $backend->writeNewPost();
-    }
-    if ($_GET['action'] == 'managePosts')
-    {
-        $backend->managePosts();
-    }
-    if ($_GET['action'] == 'manageComments')
-    {
-        $backend->manageComments();
-    } 
 }
 else {
     $frontend->listPosts();
 }
-
-// ********** Divers Test **********
-//require "view/test/testcomposer.php";
