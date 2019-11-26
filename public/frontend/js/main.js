e0c674103e70ce9beedba8e48dc18b60b297e0ef
Loann GGER <loann.goerger@glk-group.com>
2019-11-26 22:23:54 +0100

    🚀 Upgrade : New design frontend Full

--- public/frontend/js/main.js
+++ public/frontend/js/main.js
@@ -1,6 +1,7 @@
 class Application {
     constructor() {
         new Menu();
+        new Flash();
     }
 }
 
@@ -35,6 +36,31 @@ class Menu {
             this.i = false;
         });
     };
+}
+
+class Flash {
+    constructor() {
+        this.flash = document.getElementById("varFlash").innerHTML;
+        this.display();
+        this.closeManual();
+    }
+
+    display() {
+        if (this.flash.length > 0) {
+            document.getElementById("flash").style.display = "block";
+        }
+    }  
+    
+    closeManual() {
+        var that = this;
+        document.getElementById("closeFlash").addEventListener("click",() => {
+            that.closeNotification();
+        });
+    }
+
+    closeNotification() {
+        document.getElementById("flash").style.display = "none";
+    }
 }
 
 var app = new Application();
