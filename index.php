ab5b97aede4e7c9f890448eab144fd45d5d64490
Loann GGER <loann.goerger@glk-group.com>
2019-12-27 14:52:26 +0100

    ✨Review de code

--- index.php
+++ index.php
@@ -6,49 +6,45 @@ use App\Router;
 
 session_start();
 
+$dotenv = Dotenv\Dotenv::create(__DIR__);
+$dotenv->load();
+
 $action = $_GET['action'] ?? '';
 
 $router = new Router($action);
 
-
+// View FrontEnd
 $router->getRoute('', ['controller'=>'FrontendController', 'method'=>'index']);
 $router->getRoute('index', ['controller'=>'FrontendController', 'method'=>'index']);
 $router->getRoute('biographie', ['controller'=>'FrontendController', 'method'=>'author']);
-$router->getRoute('singlepost', ['controller'=>'FrontendController', 'method'=>'singlepost']);
 $router->getRoute('blog', ['controller'=>'FrontendController', 'method'=>'blog']);
+$router->getRoute('singlepost', ['controller'=>'FrontendController', 'method'=>'singlepost']);
 $router->getRoute('mentionlegales', ['controller'=>'FrontendController', 'method'=>'mentionlegales']);
 
+// View BackEnd
 $router->getRoute('admin', ['controller'=>'BackendController', 'method'=>'admin']);
-
-$router->getRoute('login', ['controller'=>'UserController', 'method'=>'login']);
-
 $router->getRoute('writeNewPost', ['controller'=>'PostController', 'method'=>'addPost']);
-$router->getRoute('managePosts', ['controller'=>'PostController', 'method'=>'managePosts']);
-$router->getRoute('delete', ['controller'=>'PostController', 'method'=>'delete']);
-
-$router->getRoute('error', ['controller'=>'ErrorController', 'method'=>'errors']);
-
-$router->getRoute('commentSignal', ['controller'=>'CommentController', 'method'=>'commentSignal']);
-$router->getRoute('manageComments', ['controller'=>'CommentController', 'method'=>'manageComments']);
-
-$router->postRoute('validelogin', ['controller'=>'UserController', 'method'=>'validelogin']);
-$router->getRoute('logout', ['controller'=>'UserController', 'method'=>'logout']);
-$router->postRoute('postComment', ['controller'=>'CommentController', 'method'=>'postComment']);
-
-$router->postRoute('post', ['controller'=>'PostController', 'method'=>'postPost']);
 $router->getRoute('viewPosts', ['controller'=>'PostController', 'method'=>'viewPosts']);
 $router->getRoute('updatePost', ['controller'=>'PostController', 'method'=>'updatePost']);
-$router->getRoute('viewdelete', ['controller'=>'PostController', 'method'=>'viewdelete']);
+$router->getRoute('manageComments', ['controller'=>'CommentController', 'method'=>'manageComments']);
 
+// Action FrontEnd
+$router->postRoute('postComment', ['controller'=>'CommentController', 'method'=>'postComment']);
+$router->getRoute('commentSignal', ['controller'=>'CommentController', 'method'=>'commentSignal']);
 
-
-$router->getRoute('voirlesposts', ['controller'=>'PostController', 'method'=>'voirlesposts']);
-
+// Action BackEnd
+$router->postRoute('post', ['controller'=>'PostController', 'method'=>'postPost']);
 $router->postRoute('updatePosts', ['controller'=>'PostController', 'method'=>'updatePosts']);
-
-$router->getRoute('viewComment', ['controller'=>'CommentController', 'method'=>'viewComment']);
+$router->getRoute('delete', ['controller'=>'PostController', 'method'=>'delete']);
 $router->getRoute('aprouvecomment', ['controller'=>'CommentController', 'method'=>'aprouvecomment']);
 $router->getRoute('refusecomment', ['controller'=>'CommentController', 'method'=>'refusecomment']);
 
+// Auth
+$router->getRoute('login', ['controller'=>'UserController', 'method'=>'login']);
+$router->postRoute('validelogin', ['controller'=>'UserController', 'method'=>'validelogin']);
+$router->getRoute('logout', ['controller'=>'UserController', 'method'=>'logout']);
+
+// Global 
+$router->getRoute('erreur', ['controller'=>'ErrorController', 'method'=>'errors']);
 
 $router->run();
